4119d78b736e12ed1c02678003ed401a633a0ee7
daverocks <david@simple-dealer.com>
2020-04-15 15:03:39 +0000

    list executions

--- app/handleGet.js
+++ app/handleGet.js
@@ -1,3 +1,32 @@
+const AWS = require('aws-sdk')
+const stepfunctions = new AWS.StepFunctions({ apiVersion: '2016-11-23' });
+
 export async function handleGet(_events) {
+  let res;
   console.log('inside get handler');
+
+  try {
+    let params = {
+      stateMachineArn: 'arn:aws:states:us-east-1:577195013332:stateMachine:FinanceAppStateMachine-da',
+      maxResults: '100',
+      // nextToken: 'STRING_VALUE',
+      // statusFilter: RUNNING | SUCCEEDED | FAILED | TIMED_OUT | ABORTED
+    };
+
+    res = await stepfunctions.listExecutions(params).promise();
+    console.log(res);
+  } catch (e) { console.log(e); }
+
+  //list available state machines
+  // try {
+  //   let params = {
+  //     maxResults: '100',
+  //     // nextToken: 'STRING_VALUE'
+  //   };
+  //   let res = await stepfunctions.listStateMachines(params).promise();
+  //   console.log(res);
+  // } catch (e) { console.log(e); }
+
+  console.log('Done.');
+  return res;
 }
